e1eed622850cd8980b1d7b678ccd6e01737fbb2b
100ta <87287616+100ta@users.noreply.github.com>
2024-08-27 16:24:03 +0900

    60

---
+++ CRM/resources/js/Pages/Customers/Index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import { onMounted } from 'vue';
+
+
+const props = defineProps({
+    customers:Object
+})
+
+onMounted(() => {
+    console.log(props.customers)
+})
+
+</script>
+
+<template>
+</template>
--- CRM/routes/web.php
+++ CRM/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\Itemontroller;
 
 
 Route::get('/', function () {
@@ -29,6 +29,9 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('items', ItemController::class)
+->middleware(['auth', 'verified']);
+
+Route::resource('customers', CustomerController::class)
 ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';

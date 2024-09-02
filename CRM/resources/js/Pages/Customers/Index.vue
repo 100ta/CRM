9ab261fe16775e55c78e31262b593f49027512ca
100ta <87287616+100ta@users.noreply.github.com>
2024-09-02 13:08:52 +0900

    61

--- CRM/resources/js/Pages/Customers/Index.vue
+++ CRM/resources/js/Pages/Customers/Index.vue
@@ -2,17 +2,23 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
 import { onMounted } from 'vue';
+import Pagination from '@/Components/Pagination.vue' 
 
 
-const props = defineProps({
+defineProps({
     customers:Object
 })
 
-onMounted(() => {
-    console.log(props.customers)
-})
+// onMounted(() => {
+//     console.log(props.customers)
+// })
 
 </script>
 
 <template>
-</template>
+    <ul v-for="customer in customers.data" :key="customer.id">
+    <li>{{ customer.id }}</li>
+    <li>{{ customer.name }}</li>
+    </ul>
+    <Pagination class="mt-6" :links="customers.links"></Pagination>
+   </template>

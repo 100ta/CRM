fb6879a4c94edbf10d82712841a429accc80e3e5
100ta <87287616+100ta@users.noreply.github.com>
2024-09-03 17:10:57 +0900

    67

--- CRM/resources/js/Pages/Customers/Create.vue
+++ CRM/resources/js/Pages/Customers/Create.vue
@@ -3,6 +3,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import { reactive } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
+import { Core as YubinBangoCore } from "yubinbango-core2";
 
 const form = reactive({
     name: null, kana: null, tel: null, email: null, postcode: null,
@@ -11,8 +12,13 @@ const form = reactive({
 
 const storeCustomer = () =>{
     Inertia.post('/customers',form);
-    console.log("aaaa")
 }
+
+const fetchAddress = () => {
+    new YubinBangoCore(String(form.postcode), (value) => {
+        form.address = value.region + value.locality + value.street
+    })
+} 
 </script>
 
 <template>
@@ -58,7 +64,7 @@ const storeCustomer = () =>{
                                         <div class="p-2 w-full">
                                         <div class="relative">
                                             <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label>
-                                            <input type="number" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                            <input type="number" id="postcode" name="postcode"  @change="fetchAddress" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                         </div>
                                         </div>
                                         <div class="p-2 w-full">
